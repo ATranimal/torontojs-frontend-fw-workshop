60e7ef1fc932283d866c93ebab9c35d09ee01c36
ATranimal <tran.andrew.a@gmail.com>
2020-03-08 01:08:12 -0500

    Adding rerendering on click

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,12 +1,18 @@
 import { p } from "../framework/elements";
 import { onClick } from "../framework/events";
+import { rerender } from "../framework/index";
 
 export const Welcome = firstName => {
-  const formattedName = firstName.toUpperCase();
+  let formattedName = firstName.toUpperCase();
 
   const popupAlert = () => {
-    alert("WOW!");
+    formattedName = formattedName + "!";
+    rerender(template(formattedName));
   };
 
-  return p(`Welcome to the workshop, ${formattedName}!`, onClick(popupAlert));
+  const template = formattedName => {
+    return p(`Welcome to the workshop, ${formattedName}!`, onClick(popupAlert));
+  };
+
+  return template(formattedName);
 };
--- framework/index.js
+++ framework/index.js
@@ -4,8 +4,14 @@ const patch = snabbdom.init([
   require("snabbdom/modules/eventlisteners").default
 ]);
 
+let oldVnode;
+
 export const init = (selector, component) => {
   const app = document.querySelector(selector);
 
-  patch(app, component.template);
+  oldVnode = patch(app, component.template);
+};
+
+export const rerender = component => {
+  oldVnode = patch(oldVnode, component.template);
 };
